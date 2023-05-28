c44b7ba7be1593188a3516bfc718d4167200a8eb
buoffio <davide.baroffio@mail.polimi.it>
2023-05-28 13:02:54 +0200

    Added last results (out_seddi_rasm_intra)

--- Testing/testing.sh
+++ Testing/testing.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-python3 testing.py registers compiled_outputs/out_seddi_cfcss.elf
-python3 testing.py memory compiled_outputs/out_seddi_cfcss.elf
+python3 testing.py registers compiled_outputs/out_seddi_rasm_intra.elf
+python3 testing.py memory compiled_outputs/out_seddi_rasm_intra.elf
